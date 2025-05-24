2c5dcf1d8dabcf22612dcdccd7bdefe0c3fefb6d
HamachiTaro <mikelito33bdx@gmail.com>
2025-05-24 18:02:01 +0900

    create project

---
+++ Source/MultiWindowCpp/Private/ExternalCameraWindowManager.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExternalCameraWindowManager.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Widgets/Images/SImage.h"
+#include "Widgets/SWindow.h"
+#include "Brushes/SlateImageBrush.h"
+#include "Framework/Application/SlateApplication.h"
+#include "Engine/Texture2D.h"
+
+void UExternalCameraWindowManager::CreateExternalWindow(UTextureRenderTarget2D* RenderTarget)
+{
+	if (RenderTarget == nullptr)
+	{
+		return;
+	}
+
+	CurrentRenderTarget = RenderTarget;
+
+	const FVector2D WindowSize(1920, 1080);
+	const FName ResourceName("ExternalWindow");
+	
+	ImageBrush = MakeShareable(new FSlateDynamicImageBrush(ResourceName, WindowSize));
+
+	ImageWidget = SNew(SImage).Image(ImageBrush.Get());
+
+	// ウィンドウ作成
+	ExternalWindow = SNew(SWindow)
+		.Title(FText::FromString("External Camera View"))
+		.ClientSize(WindowSize)
+		.SupportsMaximize(true)
+		.SupportsMinimize(true);
+
+	ExternalWindow->SetContent(ImageWidget.ToSharedRef());
+
+	// slateにウィンドウを登録
+	FSlateApplication::Get().AddWindow(ExternalWindow.ToSharedRef());
+
+	TickHandle = FTSTicker::GetCoreTicker().AddTicker(
+		FTickerDelegate::CreateUObject(this, &UExternalCameraWindowManager::Tick),
+		0.016f // 60fps
+	);
+	
+}
+
+void UExternalCameraWindowManager::CloseExternalWindow()
+{
+	if (ExternalWindow.IsValid())
+	{
+		FSlateApplication::Get().RequestDestroyWindow(ExternalWindow.ToSharedRef());
+		ExternalWindow.Reset();
+	}
+
+	ImageWidget.Reset();
+	ImageBrush.Reset();
+	CurrentRenderTarget = nullptr;
+}
+
+bool UExternalCameraWindowManager::Tick(float DeltaTime)
+{
+	if (CurrentRenderTarget == nullptr)
+	{
+		return false;
+	}
+
+	ImageBrush->SetResourceObject(CurrentRenderTarget);
+	if (ImageWidget.IsValid())
+	{
+		ImageWidget->Invalidate(EInvalidateWidgetReason::LayoutAndVolatility);
+	}
+	return true;
+}
---
+++ Source/MultiWindowCpp/Public/ExternalCameraWindowManager.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ExternalCameraWindowManager.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class MULTIWINDOWCPP_API UExternalCameraWindowManager : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	/** Render Targetを別ウィンドウに表示 **/
+	UFUNCTION(BlueprintCallable, Category = "External Window")
+	void CreateExternalWindow(UTextureRenderTarget2D* RenderTarget);
+
+	UFUNCTION(BlueprintCallable, Category = "External Window")
+	void CloseExternalWindow();
+	
+private:
+	// 表示用のウィンドウ
+	TSharedPtr<SWindow> ExternalWindow;
+	
+	/** RenderTarget を表示するための Image Brush */
+	TSharedPtr<FSlateDynamicImageBrush> ImageBrush;
+
+	/** ウィジェット（画像表示） */
+	TSharedPtr<SImage> ImageWidget;
+
+	/** RenderTarget のキャッシュ参照 */
+	UPROPERTY()
+	UTextureRenderTarget2D* CurrentRenderTarget = nullptr;
+
+	/** 定期更新用タイマー */
+	FTSTicker::FDelegateHandle TickHandle;
+
+	bool Tick(float DeltaTime);
+};
